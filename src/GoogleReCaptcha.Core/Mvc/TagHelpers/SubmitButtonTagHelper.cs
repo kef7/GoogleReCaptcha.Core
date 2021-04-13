0e151dc6b79352947d1cc8d038d1aac1b8a947ee
kef7 <as.kefka@gmail.com>
2021-04-13 18:33:53 -0400

    add, mostly trace, loging to both tag helpers; not sure they should log to info too much

--- src/GoogleReCaptcha.Core/Mvc/TagHelpers/SubmitButtonTagHelper.cs
+++ src/GoogleReCaptcha.Core/Mvc/TagHelpers/SubmitButtonTagHelper.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -30,6 +31,11 @@ namespace GoogleReCaptcha.Core.Mvc.TagHelpers
 		#endregion
 
 		#region Properties
+
+		/// <summary>
+		/// Gets the ILogger
+		/// </summary>
+		protected virtual ILogger<SubmitButtonTagHelper> Logger { get; }
 
 		/// <summary>
 		/// Gets the recaptcha settings to use for this tag's output
@@ -65,8 +71,17 @@ namespace GoogleReCaptcha.Core.Mvc.TagHelpers
 
 		#region Constructor
 
-		public SubmitButtonTagHelper(IReCaptchaV3Settings settings)
+		public SubmitButtonTagHelper(ILogger<SubmitButtonTagHelper> logger, IReCaptchaV3Settings settings)
 		{
+			if (logger == null)
+			{
+				logger = new Microsoft.Extensions.Logging.Abstractions.NullLogger<SubmitButtonTagHelper>();
+			}
+			else
+			{
+				Logger = logger;
+			}
+
 			if (settings == null)
 			{
 				throw new ArgumentNullException(nameof(settings));
@@ -83,11 +98,13 @@ namespace GoogleReCaptcha.Core.Mvc.TagHelpers
 		{
 			if (context == null)
 			{
+				Logger.LogTrace("TagHelperContext obj null for reCAPTCHA script tag");
 				throw new ArgumentNullException(nameof(context));
 			}
 
 			if (output == null)
 			{
+				Logger.LogTrace("TagHelperOutput obj null for reCAPTCHA script tag");
 				throw new ArgumentNullException(nameof(output));
 			}
 
@@ -96,33 +113,49 @@ namespace GoogleReCaptcha.Core.Mvc.TagHelpers
 			{
 				// Setup tag as non-recaptcha supported button
 				output.TagName = "button";
+				Logger.LogTrace("Suppress reCAPTCHA version of button tag");
 
 				// Leave
 				return;
 			}
 
+			Logger.LogTrace("Prepare output for reCAPTCHA button tag");
+
 			// Apply settings to props
-			SiteKey = Settings.SiteKey ?? "?";
+			if (!string.IsNullOrWhiteSpace(Settings.SiteKey))
+			{
+				Logger.LogTrace("Get SiteKey from settings");
+				SiteKey = Settings.SiteKey;
+			}
+			else
+			{
+				Logger.LogTrace("Set default SiteKey");
+				SiteKey = "?";
+			}
 
 			// Apply default action
 			if (string.IsNullOrWhiteSpace(Action))
 			{
+				Logger.LogTrace("Set default Action");
 				Action = "submit";
 			}
 
 			// Apply default callback
 			if (string.IsNullOrWhiteSpace(CallBack))
 			{
+				Logger.LogTrace("Set default Callback");
 				CallBack = "onGReCaptchaV3Submit";
 			}
 
 			// Setup tag and base google attributes
+			Logger.LogDebug("Set button tag to use {Action}, {Callback}, and {SiteKey}", Action, CallBack, SiteKey);
 			output.TagName = "button";
 			output.Attributes.SetAttribute("data-action", Action);
 			output.Attributes.SetAttribute("data-callback", CallBack);
 			output.Attributes.SetAttribute("data-sitekey", SiteKey);
 
 			// Merge class attributes with defaults and apply
+			Logger.LogTrace("Merge and set class attributes");
 			var classes = GetMergedClassAttributes(context);
 			output.Attributes.SetAttribute("class", classes);
 		}
