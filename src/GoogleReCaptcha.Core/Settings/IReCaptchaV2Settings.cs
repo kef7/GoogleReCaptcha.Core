b9126c4d661b81e60265d3596e7118c546c791a9
kef7 <as.kefka@gmail.com>
2021-06-08 14:36:38 -0400

    create root interface and implement V2 settings objects

---
+++ src/GoogleReCaptcha.Core/Settings/IReCaptchaV2Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleReCaptcha.Core.Settings
+{
+	public interface IReCaptchaV2Settings : IReCaptchaSettings
+	{
+		V2Theme Theme { get; }
+		V2Size Size { get; }
+	}
+}
