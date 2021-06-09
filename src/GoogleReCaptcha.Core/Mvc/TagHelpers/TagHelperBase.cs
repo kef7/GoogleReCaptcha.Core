b3542ff8909b4d6f476deea72c4dab9838a6461b
kef7 <as.kefka@gmail.com>
2021-06-09 09:32:39 -0400

    create tag helper for v2 widget

---
+++ src/GoogleReCaptcha.Core/Mvc/TagHelpers/TagHelperBase.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleReCaptcha.Core.Mvc.TagHelpers
+{
+	public abstract class TagHelperBase : TagHelper
+	{
+		#region Properties
+
+		/// <summary>
+		/// Gets the ILogger
+		/// </summary>
+		protected virtual ILogger Logger { get; }
+
+		#endregion
+
+		#region Constructor
+
+		public TagHelperBase(ILogger logger)
+		{
+			if (logger == null)
+			{
+				logger = new Microsoft.Extensions.Logging.Abstractions.NullLogger<TagHelperBase>();
+			}
+			Logger = logger;
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Get and merge class attributes presnt with default class attributes required for Google ReCaptcha
+		/// </summary>
+		/// <param name="context">Tag helper context of the tag being processed</param>
+		/// <returns>String of all classes from current tag context and default</returns>
+		protected string GetMergedClassAttributes(TagHelperContext context, string defaultClassAttrs)
+		{
+			var classes = defaultClassAttrs ?? "";
+			var classTagHelperAttr = context.AllAttributes["class"];
+			if (classTagHelperAttr != null)
+			{
+				var tmp = classTagHelperAttr.Value?.ToString();
+				if (!string.IsNullOrWhiteSpace(tmp))
+				{
+					classes = classes + " " + tmp;
+				}
+			}
+			return classes;
+		}
+
+		#endregion
+	}
+}
