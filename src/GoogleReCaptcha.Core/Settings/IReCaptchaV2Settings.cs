6560da1f56fd52a583d7465de93620645497748a
kef7 <kef7.dev@gmail.com>
2021-06-09 14:45:01 -0400

    udpate widget tag helper with V2 theme and size settings support

--- src/GoogleReCaptcha.Core/Settings/IReCaptchaV2Settings.cs
+++ src/GoogleReCaptcha.Core/Settings/IReCaptchaV2Settings.cs
@@ -6,7 +6,7 @@ namespace GoogleReCaptcha.Core.Settings
 {
 	public interface IReCaptchaV2Settings : IReCaptchaSettings
 	{
-		V2Theme Theme { get; }
-		V2Size Size { get; }
+		V2Theme? Theme { get; }
+		V2Size? Size { get; }
 	}
 }
